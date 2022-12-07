688e2b68ca8de265d6fea1b9b65efbd7daa1ca15
BesNull <67557818+BesNull@users.noreply.github.com>
2022-12-08 02:55:15 +0300

    Update CreateDmp.sh

--- CreateDmp.sh
+++ CreateDmp.sh
@@ -6,7 +6,7 @@ cd ~
 echo "You are in: $(pwd)"
 echo '-----------------------------------------'
 
-
 sudo PGPASSWORD=$passU psql -U $loginU -c "alter user $loginU set max_parallel_workers_per_gather=$ThreadsN;"
 echo "Threads Number changed to $ThreadsN"
+
 sudo PGPASSWORD=$passU ${pathPgDump}pg_dump -U $loginU -h $hostname -n $SchemaName -F c $Dbname > $DumpLocation
