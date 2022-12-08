cc869ad44533132829226670c8d37a56bef31031
BesNull <67557818+BesNull@users.noreply.github.com>
2022-12-08 23:57:13 +0300

    Update RestoreDmp.sh

--- RestoreDmp.sh
+++ RestoreDmp.sh
@@ -6,19 +6,30 @@ cd ~
 echo "You are in: $(pwd)"
 echo '-----------------------------------------'
 
+
 read -p 'Enter the database name: ' NewDb
 
-sudo PGPASSWORD=$passU -u $loginU psql  << XENDX  # по идее нет разницы, где указывать юзера, до или после команды
+
+#createdb -U $loginU $NewDb
+sudo PGPASSWORD=$passU -u $loginU psql  << XENDX 
 drop database if exists $NewDb;
 create database $NewDb;
-\c $NewDb;
-drop schema if exists public;
-drop schema if exists $SchemaName;
-create schema public;
-create schema $SchemaName;
 alter user $loginU set max_parallel_workers_per_gather=$ThreadsN;
 XENDX
 
+SchemaParms=""
+for item in ${SchemaName[*]}
+do
+    SchemaParms+="$item "
+    if [[ $item != '-n' ]]
+    then
+      sudo PGPASSWORD=$passU -u $loginU  psql -d $NewDb -c "drop schema if exists $item; create schema $item;"
+    fi
+
+    printf "   %s\n" $item
+done
+echo $SchemaParms
+
 echo "Threads Number changed to $ThreadsN"
 
-sudo PGPASSWORD=$passU ${pathPgRst}pg_restore -d $NewDb  -U $loginU -h $hostname -n $SchemaName  $DumpLocation
+sudo PGPASSWORD=$passU ${pathPgRst}pg_restore -d $NewDb  -U $loginU -h $hostname  $SchemaParms  $DumpLocation
