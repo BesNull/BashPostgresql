d6267e7503882a45addfc5171e5afd2a0d5d56a1
BesNull <67557818+BesNull@users.noreply.github.com>
2022-12-08 23:55:26 +0300

    Update CreateDmp.sh

--- CreateDmp.sh
+++ CreateDmp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#To-do:на некоторых соединениях не все параметры прописаны(например, хост не указан) 
 
 . ~/Scripts/'Postgresql scripts'/settings
 
@@ -6,7 +7,19 @@ cd ~
 echo "You are in: $(pwd)"
 echo '-----------------------------------------'
 
+SchemaParms=""
+space=" "
+for item in ${SchemaName[*]}
+do
+    SchemaParms+="${item}"" "
+    #printf "   %s" $item
+done
+
+echo
+echo ${SchemaParms}  #показывает немного коряво, если делать эхо без кавычек, но при полдстановке все дампит
+echo
+
 sudo PGPASSWORD=$passU psql -U $loginU -c "alter user $loginU set max_parallel_workers_per_gather=$ThreadsN;"
 echo "Threads Number changed to $ThreadsN"
-
-sudo PGPASSWORD=$passU ${pathPgDump}pg_dump -U $loginU -h $hostname -n $SchemaName -F c $Dbname > $DumpLocation
+echo $Dbname
+sudo PGPASSWORD=$passU ${pathPgDump}pg_dump -U $loginU -h $hostname ${SchemaParms} -F c $Dbname > $DumpLocation
